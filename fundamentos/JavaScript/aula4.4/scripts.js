812dd39c2b02df4d3c7071f8e9d38aad4ba3f477
Gabriel Nok <ganocchi@hotmail.com>
2022-03-10 19:32:08 -0300

    aula 4/4 salva

--- fundamentos/JavaScript/aula4.4/scripts.js
+++ fundamentos/JavaScript/aula4.4/scripts.js
@@ -96,10 +96,17 @@
 
 
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
 
-  console.log ("Boas vindas , " + info.personagem);
+
+
+// //1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   console.log ("Boas vindas , " + info.personagem);
+
+
+
